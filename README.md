0eb774df1adf296bc28e03cad9f519e25946bf66
samuel-pratt <sjpratt19@gmail.com>
2020-11-29 00:57:01 -0800

    Added support for Manifolds, upload, download, fuel engines, and divert power

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # Among Us Autoclicker
 
-Basic python program that will complete tasks in the game among us.
+Basic python program that will complete tasks in the game among us. It works on a simple system of find and click, drag, or hold. It runs in the background and as you open tasks, it will complete them for you.
 This is just for fun, please don't use this to make the game unfair to others.
 
 ### Currently supported tasks
 
-None, I j ust started this
+- Unlock manifolds
+- Upload / Download
+- Fuel engines
+- Divert power
 
 ### Tasks I'm working on
 
 - Destroy Asteroids
-- Unlock manifolds
-- Upload / Download
+- Calibrate shields
+- Card swipe
+- Garbage dump
+- Wires
+
+### ToDo
+
+- Optimize what tasks the program checks for, for example it doesn't need to check for manifolds unless player is in the reactor
+- Add text recognition for current room, active tasks
+- Clean up code a bit, variable names and files are a bit all over the place / redundant
--- main.py
+++ main.py
@@ -1,16 +1,85 @@
 import pyautogui
+import time
 
-from screen_search import *
+while(True):
+    isDownloadVisible = pyautogui.locateOnScreen("./images/uploaddata/download.png",  grayscale=True, confidence=0.9)
+    isUploadVisible = pyautogui.locateOnScreen("./images/uploaddata/upload.png", grayscale=True, confidence=0.9)
+    manifolds1 = pyautogui.locateOnScreen("./images/manifolds/1.png", grayscale=True, confidence=0.9)
+    fuelEngines = pyautogui.locateOnScreen("./images/fuel/fuelButton.png", grayscale=True, confidence=0.9)
+    divertPower1 = pyautogui.locateOnScreen("./images/divertPower/divertPower1.png", confidence=0.9)
+    divertPower2 = pyautogui.locateOnScreen("./images/divertPower/divertPower2.png", confidence=0.9)
+    swipeCard = pyautogui.locateOnScreen("./images/swipeCard/card.png", confidence=0.9)
 
-# Search for the github logo on the whole screen
-# note that the search only works on your primary screen.
-search = Search("./images/manifolds/1.png")
+    if swipeCard:
+        clickLocation = pyautogui.center(swipeCard)
+        pyautogui.click(clickLocation)
+        time.sleep(2)
+        swipeCard = pyautogui.locateOnScreen("./images/swipeCard/swipe.png", grayscale=True, confidence=0.9)
+        clickLocation = pyautogui.center(swipeCard)
+        pyautogui.click(clickLocation)
+        pyautogui.dragRel(1500, 0, duration=1.5)
+
+    if divertPower2:
+        clickLocation = pyautogui.center(divertPower2)
+        pyautogui.click(clickLocation)
+
+    if divertPower1:
+        pyautogui.click(divertPower1)
+        pyautogui.dragRel(0, -200, duration=0.2)
+
+    if fuelEngines:
+        clickLocation = pyautogui.center(fuelEngines)
+        pyautogui.mouseDown(clickLocation)
+        time.sleep(3)
+        pyautogui.mouseUp()
+
+    if manifolds1:
+        clickLocation = pyautogui.center(manifolds1)
+        pyautogui.click(clickLocation)
+
+        manifolds2 = pyautogui.locateOnScreen("./images/manifolds/2.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds2)
+        pyautogui.click(clickLocation)
+
+        manifolds3 = pyautogui.locateOnScreen("./images/manifolds/3.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds3)
+        pyautogui.click(clickLocation)
+
+        manifolds4 = pyautogui.locateOnScreen("./images/manifolds/4.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds4)
+        pyautogui.click(clickLocation)
+
+        manifolds5 = pyautogui.locateOnScreen("./images/manifolds/5.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds5)
+        pyautogui.click(clickLocation)
+
+        manifolds6 = pyautogui.locateOnScreen("./images/manifolds/6.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds6)
+        pyautogui.click(clickLocation)
+
+        manifolds7 = pyautogui.locateOnScreen("./images/manifolds/7.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds7)
+        pyautogui.click(clickLocation)
+
+        manifolds8 = pyautogui.locateOnScreen("./images/manifolds/8.png",confidence=0.9)
+        clickLocation = pyautogui.center(manifolds8)
+        pyautogui.click(clickLocation)
+
+        manifolds9 = pyautogui.locateOnScreen("./images/manifolds/9.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds9)
+        pyautogui.click(clickLocation)
+
+        manifolds10 = pyautogui.locateOnScreen("./images/manifolds/10.png", confidence=0.9)
+        clickLocation = pyautogui.center(manifolds10)
+        pyautogui.click(clickLocation)
 
 
-pos = search.imagesearch()
+    if isDownloadVisible:
+        clickLocation = pyautogui.center(isDownloadVisible)
 
-if pos[0] != -1:
-    print("position : ", pos[0], pos[1])
-    pyautogui.moveTo(pos[0], pos[1])
-else:
-    print("image not found")
+        pyautogui.click(clickLocation)
+
+    if isUploadVisible:
+        clickLocation = pyautogui.center(isUploadVisible)
+
+        pyautogui.click(clickLocation)
